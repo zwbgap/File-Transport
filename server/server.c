016a5a10c0861290478c70ad866ceeb2a087b8ce
ZAM <z2634541542@gmail.com>
2024-06-17 22:46:35 +0800

    update

---
+++ server/server.c
@@ -0,0 +1,51 @@
+#include "server_work.h"
+
+int listenfd;
+
+int main(){
+    /** 创建广播线程 **/
+    pthread_t broadthrd;
+    int err=-1;
+    if (( (err = pthread_create(&broadthrd,NULL,broad,NULL))) !=0 ){
+        perror("can't create broad thread");
+        exit(-1);
+    }
+
+    listenfd = Server_init(TCP);
+    
+    /** 接收客户端命令行参数 **/
+    struct command cmd;
+    while(1){
+        printf("\nWait for task...\n");
+        bzero(&cmd,sizeof(struct command));
+        int sockfd = recv_cmd(&cmd);
+        printf("cmd:%s -%s -%s\n",cmd.filename,cmd.cmd,cmd.mode); 
+        
+        if(get_cmd(cmd.filename) == LS){
+            //printf("ls1\n");
+            send_file_list(sockfd);
+            //system("ls");
+            continue;
+        }
+        recv_fileinfo(&cmd); 
+        switch (get_cmd(cmd.cmd)){
+
+            case GET:
+                /** 客户端下载请求 **/
+                send_file(&cmd);
+                break;
+            case PUT:
+                /** 客户端上传请求 **/
+                recv_block(&cmd);
+                break;
+            case UNKNOWN:
+            default:
+                printf("cmd error\n");
+                break;
+        }
+    }
+
+    return 0;
+}
+
+
